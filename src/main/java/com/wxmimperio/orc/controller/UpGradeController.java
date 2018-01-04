3495023bb2b93f3aa833e364c01bd4948448dc96
wxmimperio <wxmimperio@outlook.com>
2018-01-04 16:55:25 +0800

    return

--- src/main/java/com/wxmimperio/orc/controller/UpGradeController.java
+++ src/main/java/com/wxmimperio/orc/controller/UpGradeController.java
@@ -6,27 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class UpGradeController {
 
     @Autowired
     private SequenceToOrcDaoImpl sequenceToOrcDao;
 
-    @RequestMapping(value = "/upgrade/getAllTables", method = RequestMethod.GET, consumes = "application/json")
-    public void getAllTables() throws Exception {
-
-        System.out.println(sequenceToOrcDao.getAll());
+    @RequestMapping(value = "/upgrade/get/getAllTables", method = RequestMethod.GET, consumes = "application/json")
+    public List<SequenceToOrcInfo> getAllTables() throws Exception {
+        return sequenceToOrcDao.getAll();
     }
 
-    @RequestMapping(value = "/upgrade/sequenceToOrcInfo", method = RequestMethod.POST, consumes = "application/json")
+    @RequestMapping(value = "/upgrade/update/sequenceToOrcInfo", method = RequestMethod.POST, consumes = "application/json")
     public void update(@RequestBody SequenceToOrcInfo sequenceToOrcInfo) throws Exception {
-
-        System.out.println(sequenceToOrcInfo);
+        sequenceToOrcDao.update(sequenceToOrcInfo);
     }
 
     @RequestMapping(value = "/upgrade/getTopicByName/{tableName}", method = RequestMethod.GET, consumes = "application/json")
     public SequenceToOrcInfo getTopicByName(@PathVariable String tableName) throws Exception {
-        SequenceToOrcInfo sequenceToOrcInfo = sequenceToOrcDao.getTopicByName(tableName);
-        return sequenceToOrcInfo;
+        return sequenceToOrcDao.getTopicByName(tableName);
     }
 }
